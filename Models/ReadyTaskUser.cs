b51cf50b0444c6c9a19e16f4ee856160fbf69db3
Caleb M Breaux <814196499@wwcc.edu>
2019-10-01 10:55:07 -0700

    Add project files.

---
+++ Models/ReadyTaskUser.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReadyTask.Models
+{
+    public class ReadyTaskUser : IdentityUser<int>
+    {
+        [PersonalData, StringLength(20)]
+        public string FirstName { get; set; }
+        [PersonalData, StringLength(20)]
+        public string LastName { get; set; }
+    }
+}
